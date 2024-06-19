1c74a0498857d51a480f4bada5669b55da833122
unit-4o4 <conway24b@gmail.com>
2024-06-19 16:22:53 +0700

    use index.html in src

--- vite.config.ts
+++ vite.config.ts
@@ -1,42 +1,26 @@
-/**
- * Babel will compile modern JavaScript down to a format compatible with older browsers, but it will also increase your
- * final bundle size and build speed. Edit the `browserslist` property in the package.json file to define which
- * browsers Babel should target.
- *
- * Browserslist documentation: https://github.com/browserslist/browserslist#browserslist-
- */
 const useBabel = true;
+const sourceMapsInProduction = true;
 
-/**
- * Change this to `true` to generate source maps alongside your production bundle. This is useful for debugging, but
- * will increase total bundle size and expose your source code.
- */
-const sourceMapsInProduction = false;
-/**********************, { replace, type postcss }***********************************************************************************************/
-/**********                                              Vite                                               **********/
-/*********************************************************************************************************************/
-
-import { defineConfig, UserConfig } from 'vite';
+//
 import { svelte } from '@sveltejs/vite-plugin-svelte';
-import { env } from 'process';
-import dist from 'svelte-preprocess';
 import { type UserConfig, defineConfig, type build } from 'vite';
 import path from 'path';
 import sveltePreprocess from 'svelte-preprocess';
 import legacy from '@vitejs/plugin-legacy';
-import autoprefixer from 'autoprefixer';
 import pkg from './package.json';
 import tsconfig from './tsconfig.json';
 import { VitePWA } from 'vite-plugin-pwa'
+import autoprefixer from 'autoprefixer';
 
-
+//
 const production = process.env.NODE_ENV === 'production';
 const config = <UserConfig> defineConfig({
-	plugins: [
-		svelte({
-			emitCss: production,
-			preprocess: sveltePreprocess(),
-		}),
+    root: "./src",
+    plugins: [
+        svelte({
+            emitCss: production,
+            preprocess: sveltePreprocess(),
+        }),
         VitePWA({ 
             registerType: 'autoUpdate',
             devOptions: {
@@ -47,51 +31,56 @@ const config = <UserConfig> defineConfig({
                 skipWaiting: true
             }
         })
-	],
-	server: {
-		host: 'localhost',
-		port: 5000,
-	},
-	build: {
-		sourcemap: sourceMapsInProduction,
-		outDir: './',
-	},
-	css: {
-		postcss: {
-			plugins: [
-				autoprefixer(),
-			],
-		},
-	},
+    ],
+    server: {
+        open: './index.html',
+        host: 'localhost',
+        port: 5000,
+    },
+    build: {
+        sourcemap: sourceMapsInProduction,
+        outDir: '../dist',
+        emptyOutDir: true,
+        rollupOptions: {
+            input: "./index.html"
+        },
+    },
+    css: {
+        postcss: {
+            plugins: [
+                autoprefixer(),
+            ],
+        },
+    },
 });
 
 // Babel
 if (useBabel) {
-	config.plugins?.unshift(
-		legacy({
-			targets: pkg.browserslist,
-		}),
-	);
+    config.plugins?.unshift(
+        legacy({
+            targets: pkg.browserslist,
+        }),
+    );
 }
 
 // Load path aliases from the tsconfig.json file
 const aliases = tsconfig.compilerOptions.paths;
 
 for (const alias in aliases) {
-	const paths = aliases[alias].map((p: string) => path.resolve(__dirname, p));
+    const paths = aliases[alias].map((p: string) => path.resolve(__dirname, p));
 
-	// Our tsconfig uses glob path formats, whereas vite just wants directories
-	// We'll need to transform the glob format into a format acceptable to vite
+    // Our tsconfig uses glob path formats, whereas vite just wants directories
+    // We'll need to transform the glob format into a format acceptable to vite
 
-	const viteAlias = alias.replace(/(\\|\/)\*$/, '');
-	const vitePaths = paths.map((p: string) => p.replace(/(\\|\/)\*$/, ''));
+    const viteAlias = alias.replace(/(\\|\/)\*$/, '');
+    const vitePaths = paths.map((p: string) => p.replace(/(\\|\/)\*$/, ''));
 
-	if (!config.resolve) config.resolve = {};
-	if (!config.resolve.alias) config.resolve.alias = {};
+    if (!config.resolve) config.resolve = {};
+    if (!config.resolve.alias) config.resolve.alias = {};
 
-	if (config.resolve && config.resolve.alias && !(viteAlias in config.resolve.alias)) {
-		config.resolve.alias[viteAlias] = vitePaths.length > 1 ? vitePaths : vitePaths[0];
-	}
+    if (config.resolve && config.resolve.alias && !(viteAlias in config.resolve.alias)) {
+        config.resolve.alias[viteAlias] = vitePaths.length > 1 ? vitePaths : vitePaths[0];
+    }
 }
 
 export default config;
