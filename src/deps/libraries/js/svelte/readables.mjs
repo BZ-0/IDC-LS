574ac390475d8f180ce277ae3b042bf638cfe0bf
unit-4o4 <conway24b@gmail.com>
2024-06-21 18:42:15 +0700

    new readable

--- src/deps/libraries/js/svelte/readables.mjs
+++ src/deps/libraries/js/svelte/readables.mjs
@@ -29,3 +29,32 @@ export const orientationAngle = () => {
         });
     });
 };
+
+// for listen settings window size (use it only when mount)
+export const whenResize = (element, box = "border-box") => {
+    if (!element) return null;
+    return readable(
+        [element?.clientWidth || 0, element?.clientHeight || 0],
+        (set) => {
+            //
+            const obs = new ResizeObserver(() => {
+                for (const entry of entries) {
+                    const box =
+                        entry[
+                            box == "border-box"
+                                ? "borderBoxSize"
+                                : "contentBoxSize"
+                        ][0];
+                    if (box) {
+                        set([box.inlineSize, box.blockSize]);
+                    }
+                }
+            });
+
+            //
+            if (element) {
+                obs.observe(element, { box });
+            }
+        }
+    );
+};
