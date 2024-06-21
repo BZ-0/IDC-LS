bc71c86d4388fb93e315e1a38eac36f16a971aa0
unit-4o4 <conway24b@gmail.com>
2024-06-21 20:37:07 +0700

    WIP new settings layout

--- src/deps/modules/svelte/decors/area.svelte
+++ src/deps/modules/svelte/decors/area.svelte
@@ -1,19 +1,21 @@
-<div class="area-decor">
+<div class="area-decor" {...$$props}>
     <div class="icon-box hl-1">
-        
+        <slot name="icon"></slot>
     </div>
-    <div class="content-box">
-        <div class="label-box">
-            <slot></slot>
-        </div>
-        <div class="in-element">
-            <slot name="element"></slot>
-        </div>
+    
+    <div class="label-box">
+        <slot></slot>
     </div>
+    
+    <div class="in-element">
+        <slot name="element"></slot>
+    </div>
+
     <!--<div class="description">
     
     </div>-->
 </div>
+
 
 <style type="scss">
     //
@@ -22,9 +24,28 @@
         inline-size: stretch;
         block-size: max-content;
         
+        //
         display: grid;
+        
+        //
         grid-template-columns: minmax(2rem, max-content) minmax(0px, 1fr) minmax(2rem, max-content);
         grid-template-rows: minmax(2rem, max-content);
+        
+        //
+        & .icon-box   { grid-column: 1 / 1; grid-row: 1 / 1; }
+        & .label-box  { grid-column: 2 / 2; grid-row: 1 / 1; }
+        & .in-element { grid-column: 3 / 3; grid-row: 1 / 1; }
+        
+        //
+        &.area-alt {
+            grid-template-columns: minmax(2rem, max-content) minmax(0px, 1fr);
+            grid-template-rows: minmax(2rem, max-content) minmax(2rem, max-content);
+            
+            //
+            & .icon-box   { grid-column: 1 / 1;  grid-row: 1 / 2; }
+            & .in-element { grid-column: 2 / 2;  grid-row: 2 / 2; }
+            & .label-box  { grid-column: 2 / 2; grid-row: 1 / 1; }
+        }
         
         //
         .icon-box {
--- src/deps/modules/svelte/settings/Settings.svelte
+++ src/deps/modules/svelte/settings/Settings.svelte
@@ -1,6 +1,7 @@
 <script>
 	import { settings } from "grid-state.mjs";
 	import RangeSlider from 'svelte-range-slider-pips';
+	import Area from './../decors/Area.svelte';
 
 	//
 	export let columns = writable([4]);
@@ -12,21 +13,29 @@
 	rows.subscribe(([v])=>{ settings.rows.set(v); })
 </script>
 
-<div class="lx-settings">
+<div class="lx-settings" data-current-page={currentPage}>
 
 	<div class="titlebar">
 		
 	</div>
 	<div class="content">
 		<div class="lx-page lx-tabs">
-			
+			<Area class="area-decor lx-tab-item">
+				
+			</Area>
 		</div>
 		<div class="lx-page lx-options">
 			
 			{#if ["grid-settings"].indexOf(currentPage) >= 0}
 				<form data-page data-name="grid-columns-row">
-					<RangeSlider s-class="slider" bind:values={columns} min={4} max={6} step={1}></RangeSlider>
-					<RangeSlider s-class="slider" bind:values={rows} min={8} max={12} step={1}></RangeSlider>
+					<Area class="area-decor area-alt">
+						<div class="opt-label"></div>
+						<RangeSlider slot="element" s-class="slider" bind:values={columns} min={4} max={6} step={1}></RangeSlider>
+					</Area>
+					<Area class="area-decor area-alt">
+						<div class="opt-label"></div>
+						<RangeSlider slot="element" s-class="slider" bind:values={rows} min={8} max={12} step={1}></RangeSlider>
+					</Area>
 				</form>
 			{/if}
 			
