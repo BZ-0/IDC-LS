d50699cf8a09332789ac2f46d674ebdff21d9cd9
unit-4o4 <conway24b@gmail.com>
2024-06-25 15:46:21 +0700

    WIP icon-edit

--- src/deps/workers/actionRegistry.mjs
+++ src/deps/workers/actionRegistry.mjs
@@ -1,3 +1,12 @@
+import { focusIconForEdit, gridState } from "@states/gridState.mjs";
+
+//
+export let onFocus = {
+    iconItem: focusIconForEdit(""),
+    iconItemId: writable(""),
+};
+
+//
 export const actionRegistry = new Map([
     [
         "default",
@@ -11,12 +20,56 @@ export const actionRegistry = new Map([
             window.open(from.dataset.href, "_blank");
         },
     ],
+    [
+        "edit-icon",
+        (from, event) => {
+            const id = from?.dataset?.id;
+            if (id) {
+                onFocus.iconItem = focusIconForEdit(id);
+                onFocus.iconItemId.set(id);
+            }
+        },
+    ],
+    [
+        "delete-icon",
+        (from, event) => {
+            const id = from?.dataset?.id || onFocus?.focusIconState?.id;
+            if (id) {
+                gridState.iconItems.delete(id);
+                gridState.iconItems = gridState.iconItems;
+
+                // do lost focus when deleted
+                onFocus.iconItem = focusIconForEdit("");
+                onFocus.iconItemId.set("");
+            }
+
+            //
+            const currentIconGrid =
+                from?.closest('.icon-grid[data-type="icons"]') ||
+                document.querySelector('.icon-grid[data-type="icons"]');
+
+            //
+            const currentGridId = currentIconGrid.dataset.id;
+        },
+    ],
+    [
+        "create-icon",
+        (from, event) => {
+            //
+            const currentIconGrid =
+                from?.closest('.icon-grid[data-type="icons"]') ||
+                document.querySelector('.icon-grid[data-type="icons"]');
+
+            //
+            const currentGridId = currentIconGrid.dataset.id;
+        },
+    ],
 ]);
 
 // support of actions
-document.addEventListener("click", (ev)=>{
+document.addEventListener("click", (ev) => {
     const el = ev.target;
-    const pr = el.matches("[data-action]") ? el : el.closest("[data-action]")
+    const pr = el.matches("[data-action]") ? el : el.closest("[data-action]");
     if (pr?.dataset?.action) {
         actionRegistry.get(pr.dataset.action)?.(pr, ev);
     }
