f64f84ceb58b8893f79468fd74f9e4d2bc2c40c6
unit-404 <conway24b@gmail.com>
2024-07-18 22:50:00 +0700

    needs to make download wallpaper

--- src/Manager.svelte
+++ src/Manager.svelte
@@ -13,6 +13,9 @@
     import { fade } from "svelte/transition";
 
     //
+    import { pickWallpaperImage } from "./ActionMap.ts";
+
+    //
     let selectedFilename = null;
     let files = new Map([]);
     
@@ -55,6 +58,7 @@
             selectedFilename = ev.target.dataset.filename;
         }
         
+        //
         if (ev.target.matches("#manager .use-item")) {
             if (selectedFilename && files.has(selectedFilename)) {
                 const file = files.get(selectedFilename);
@@ -64,6 +68,49 @@
                     wallpaper?.["$useImageAsSource"]?.(file);
                 }
             }
+        }
+        
+        //
+        if (ev.target.matches("#manager .add-item")) {
+            pickWallpaperImage()
+                .catch(console.warn.bind(console))
+                .then(async (blob) => {
+                    if (blob) {
+                        //
+                        const root = await navigator?.storage?.getDirectory?.();
+                        const wall = await root?.getDirectoryHandle?.("images");
+                        
+                        //
+                        const fn = blob?.name || "wallpaper";
+                        const fw = await (await wall?.getFileHandle?.(fn, {
+                            create: true,
+                        }))?.createWritable?.();
+                        
+                        //
+                        await fw?.write?.(blob);
+                        await fw?.flush?.();
+                        await fw?.close?.();
+                        
+                        //
+                        files.set(fn, blob);
+                        fileList.set(files);
+                    }
+                });
+        }
+        
+        //
+        if (ev.target.matches("#manager .remove-item") && selectedFilename) {
+            (async ()=>{
+                if (("/opfs?path=images/" + (selectedFilename || "wallpaper")) != localStorage.getItem("@wallpaper")) {
+                    const root = await navigator?.storage?.getDirectory?.();
+                    const wall = await root?.getDirectoryHandle?.("images");
+                    await wall?.removeEntry?.(selectedFilename);
+                    
+                    //
+                    files.delete(selectedFilename);
+                    fileList.set(files);
+                }
+            })();
         }
     });
 
@@ -79,8 +126,23 @@
 
     <div class="ls-nav ux-solid hl-1">
         <button class="use-item hl-1 hl-2h">
-            <div inert={true} class="icon"><LucideIcon slot="icon" name={"wallpaper"}/></div>
+            <div inert={true} class="icon"><LucideIcon slot="icon" name={"image-play"}/></div>
             <div inert={true} class="name">Use as Wallpaper</div>
+        </button>
+        
+        <button class="add-item hl-1 hl-2h">
+            <div inert={true} class="icon"><LucideIcon slot="icon" name={"image-up"}/></div>
+            <div inert={true} class="name">Load Image</div>
+        </button>
+        
+        <button class="remove-item hl-1 hl-2h">
+            <div inert={true} class="icon"><LucideIcon slot="icon" name={"image-off"}/></div>
+            <div inert={true} class="name">Remove Image</div>
+        </button>
+        
+        <button class="download-item hl-1 hl-2h">
+            <div inert={true} class="icon"><LucideIcon slot="icon" name={"image-down"}/></div>
+            <div inert={true} class="name">Download Image</div>
         </button>
     </div>
     <x-scrollbox class="ux-space" transition:fade={{ key: currentPage, delay: 0, duration: 100 }}>
