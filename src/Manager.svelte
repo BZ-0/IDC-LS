65117754d8b10696dd062f426fd6dcecec5d2cc6
unit-404 <conway24b@gmail.com>
2024-07-18 22:56:14 +0700

    complete saga

--- src/Manager.svelte
+++ src/Manager.svelte
@@ -44,6 +44,66 @@
         return files;
     }
 
+
+
+    // Function to download data to a file
+    export const downloadImage = async (file) => {
+        const filename = file.name || "wallpaper";
+    
+        //
+        if ("msSaveOrOpenBlob" in self.navigator) {
+            // IE10+
+            // @ts-ignore
+            window.navigator.msSaveOrOpenBlob(file, filename);
+        }
+    
+        //
+        // @ts-ignore
+        const fx = await (self?.showOpenFilePicker
+            ? new Promise((r) =>
+                r({
+                    // @ts-ignore
+                    showOpenFilePicker: self?.showOpenFilePicker?.bind?.(window),
+                    // @ts-ignore
+                    showSaveFilePicker: self?.showSaveFilePicker?.bind?.(window),
+                    // @ts-ignore
+                })
+            )
+            : import("@unite/polyfill/showOpenFilePicker.mjs"));
+    
+        //
+        // @ts-ignore
+        if (window?.showSaveFilePicker) {
+            // @ts-ignore
+            const fileHandle = await self
+                ?.showSaveFilePicker?.({
+                    suggestedName: filename
+                })
+                ?.catch?.(console.warn.bind(console));
+            const writableFileStream = await fileHandle
+                ?.createWritable?.()
+                ?.catch?.(console.warn.bind(console));
+            await writableFileStream
+                ?.write?.(file)
+                ?.catch?.(console.warn.bind(console));
+            await writableFileStream?.close()?.catch?.(console.warn.bind(console));
+        } else {
+            // Others
+            let url = "";
+            const a = document.createElement("a");
+            a.href = url = URL.createObjectURL(file);
+            a.download = filename;
+            document.body.appendChild(a);
+            a.click();
+            setTimeout(function () {
+                document.body.removeChild(a);
+                window.URL.revokeObjectURL(url);
+            }, 0);
+        }
+    };
+
+
+
     //
     document.documentElement.addEventListener("click", (ev)=>{
         if (ev.target.matches("#manager .file")) {
@@ -111,6 +171,13 @@
                     fileList.set(files);
                 }
             })();
+        }
+        
+        //
+        if (ev.target.matches("#manager .download-item") && selectedFilename) {
+            if (selectedFilename && files.has(selectedFilename)) {
+                downloadImage(files.get(selectedFilename));
+            }
         }
     });
 
