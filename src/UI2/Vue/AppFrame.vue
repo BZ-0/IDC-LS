84b37fff40345b8b9db1a52a2c917fa6e22caa49
unit-404 <conway24b@gmail.com>
2024-07-24 19:52:06 +0700

    much simpler theme changer

--- src/UI2/Vue/AppFrame.vue
+++ src/UI2/Vue/AppFrame.vue
@@ -16,13 +16,13 @@
     <div :data-hidden="chash != props.hashIdName" data-scheme="solid" class="ux-frame ux-app-frame ux-default-theme ux-detached" v-bind="$attrs">
 
         <div class="titlebar" data-highlight="2" data-scheme="solid">
-            <div data-transparent class="back-button" style="grid-column: back-button; aspect-ratio: 1 / 1;">
+            <div data-scheme="solid" data-highlight="2" class="back-button" style="grid-column: back-button; aspect-ratio: 1 / 1;">
                 <LucideIcon inert name="chevron-down"/>
             </div>
-            <div data-transparent class="ux-title-handle" style="background-color: transparent;">
+            <div data-scheme="solid" data-highlight="2" class="ux-title-handle">
 
             </div>
-            <div data-transparent class="menu-button" style="grid-column: menu-button; aspect-ratio: 1 / 1;">
+            <div data-scheme="solid" data-highlight="2" class="menu-button" style="grid-column: menu-button; aspect-ratio: 1 / 1;">
                 <LucideIcon inert name="menu"/>
             </div>
         </div>
